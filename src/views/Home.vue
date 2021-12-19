067e1bf4ad7b5cab2f832584a0a0e69cc5a5c5af
lucasDechenier <lucas.dechenieroliveira@hotmail.com>
2021-12-19 11:08:27 -0300

    style: :art: Estiliza informações superiores.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,10 @@
 <template>
   <section class="base">
-    <div>
-      <div>
-        <span>Seg / sex - 08:00 ás 18:00</span>
-        <a href="tel:+5583999037571">+55 83 99903-7571</a>
-        <span>Rua Pastor Jonathas Barros de Oliveira, 30, João Pessoa - PB</span>
+    <div class="bg-info d-flex pt-3">
+      <div class="d-flex color-white max-w-960 justify-between flex-grow flex-wrap">
+        <span class="mx-3 mb-3">Seg / sex - 08:00 ás 18:00</span>
+        <a href="tel:+5583999037571" class="color-white mx-3 mb-3">+55 83 99903-7571</a>
+        <span class="mx-3 mb-3">Rua Jonathas Barros de oliveira, 30 ,João Pessoa - PB</span>
       </div>
     </div>
 
@@ -14,12 +14,12 @@
           <router-link to="/">FlexBlog</router-link>
         </div>
         <nav>
-          <ul>
-            <li><a href="#sobre">Sobre</a></li>
-            <li><router-link to="/produtos">Produtos</router-link></li>
-            <li><router-link to="/preco">Preço</router-link></li>
-            <li><router-link to="/qualidade">qualidade</router-link></li>
-          </ul>
+          <div>
+            <span><a href="#sobre">Sobre</a></span>
+            <span><a href="#produtos">Produtos</a></span>
+            <span><a href="#preco">Preço</a></span>
+            <span><a href="#qualidade">qualidade</a></span>
+          </div>
         </nav>
       </div>
     </header>
@@ -39,10 +39,10 @@
           condições inegavelmente apropriadas objetivos.</span>
       </div>
       <div>
-        <v-img :src="require('../assets/sobre1.jpg')" max-height="550" max-width="220" contain></v-img>
+        <v-img :src="require('../assets/sobre1.jpg')" max-height="550" max-width="100%" contain></v-img>
       </div>
       <div>
-        <v-img :src="require('../assets/sobre2.jpg')" max-height="550" max-width="220" contain></v-img>
+        <v-img :src="require('../assets/sobre2.jpg')" max-height="550" max-width="100%" contain></v-img>
       </div>
     </section>
 
@@ -51,15 +51,15 @@
       <div>
         <div>
           <h2>Purple</h2>
-          <v-img :src="require('../assets/produtos1.jpg')" max-height="550" max-width="220" contain></v-img>
+          <v-img :src="require('../assets/produtos1.jpg')" max-height="550" max-width="100%" contain></v-img>
         </div>
         <div>
           <h2>Pink</h2>
-          <v-img :src="require('../assets/produtos2.jpg')" max-height="550" max-width="220" contain></v-img>
+          <v-img :src="require('../assets/produtos2.jpg')" max-height="550" max-width="100%" contain></v-img>
         </div>
          <div>
           <h2>Blue</h2>
-          <v-img :src="require('../assets/produtos3.jpg')" max-height="550" max-width="220" contain></v-img>
+          <v-img :src="require('../assets/produtos3.jpg')" max-height="550" max-width="100%" contain></v-img>
         </div>
       </div>
     </section>
@@ -68,39 +68,39 @@
       <div>
         <h2>Cobre</h2>
         <span><sup>R$</sup>19</span>
-        <ul>
-          <li>Planos ilimitados</li>
-          <li>Acesso restrito</li>
-          <li>Cateúdo secreto</li>
-          <li>Suporte 24h</li>
-        </ul>
+        <div>
+          <span>Planos ilimitados</span>
+          <span>Acesso restrito</span>
+          <span>Cateúdo secreto</span>
+          <span>Suporte 24h</span>
+        </div>
         <a href="#">Comprar</a>
       </div>
 
-      <div>
+      <div id="preco">
         <h2>Prata</h2>
         <span><sup>R$</sup>39</span>
-        <ul>
-          <li>Planos ilimitados</li>
-          <li>Acesso restrito</li>
-          <li>Cateúdo secreto</li>
-          <li>Suporte 24h</li>
-          <li>Compra exclusiva</li>
-        </ul>
+        <div>
+          <span>Planos ilimitados</span>
+          <span>Acesso restrito</span>
+          <span>Cateúdo secreto</span>
+          <span>Suporte 24h</span>
+          <span>Compra exclusiva</span>
+        </div>
         <a href="#">Comprar</a>
       </div>
 
       <div>
         <h2>Ouro</h2>
         <span><sup>R$</sup>79</span>
-        <ul>
-          <li>Planos ilimitados</li>
-          <li>Acesso restrito</li>
-          <li>Cateúdo secreto</li>
-          <li>Suporte 24h</li>
-          <li>Suporte 24h</li>
-          <li>Download dos itens</li>
-        </ul>
+        <div>
+          <span>Planos ilimitados</span>
+          <span>Acesso restrito</span>
+          <span>Cateúdo secreto</span>
+          <span>Suporte 24h</span>
+          <span>Suporte 24h</span>
+          <span>Download dos itens</span>
+        </div>
         <a href="#">Comprar</a>
       </div>
     </section>
@@ -108,45 +108,45 @@
     <section id="qualidade">
       <div>
         <h2>Inteligente</h2>
-        <p>O que temos que ter sempre em mente é que a 
-        determinação clara de objetivos afeta positivamente a correta previsão</p>
+        <span>O que temos que ter sempre em mente é que a 
+        determinação clara de objetivos afeta positivamente a correta previsão</span>
       </div>
 
       <div>
         <h2>Compacto</h2>
-        <p>O que temos que ter sempre em mente é que a determinação clara de objetivos afeta 
-          positivamente a correta previsão das condições inegavelmente</p>
+        <span>O que temos que ter sempre em mente é que a determinação clara de objetivos afeta 
+          positivamente a correta previsão das condições inegavelmente</span>
       </div>
 
       <div>
         <h2>Econômico</h2>
-        <p>O que temos que ter sempre em mente é que a determinação clara de objetivos 
-          afeta positivamente a correta previsão</p>
+        <span>O que temos que ter sempre em mente é que a determinação clara de objetivos 
+          afeta positivamente a correta previsão</span>
       </div>
 
       <div>
         <h2>Transparente</h2>
-        <p>O que temos que ter sempre em mente é que a determinação clara de 
-          objetivos afeta positivamente a correta previsão</p>
+        <span>O que temos que ter sempre em mente é que a determinação clara de 
+          objetivos afeta positivamente a correta previsão</span>
       </div>
 
       <div>
         <h2>Opaco</h2>
-        <p>O que temos que ter sempre em mente é que a determinação clara de objetivos 
-          afeta positivamente a correta previsão das condições inegavelmente</p>
+        <span>O que temos que ter sempre em mente é que a determinação clara de objetivos 
+          afeta positivamente a correta previsão das condições inegavelmente</span>
       </div>
 
       <div>
         <h2>Sustentável</h2>
-        <p>O que temos que ter sempre em mente é que a determinação clara de objetivos 
-          afeta positivamente a correta previsão</p>
+        <span>O que temos que ter sempre em mente é que a determinação clara de objetivos 
+          afeta positivamente a correta previsão</span>
       </div>
     </section>
 
     <section>
       <div>
         <h1>Newsletter</h1>
-        <p>Assine e fique por dentro das novidades</p>
+        <span>Assine e fique por dentro das novidades</span>
       </div>
       <form>
         <input type="text" placeholder="Seu e-mail">
